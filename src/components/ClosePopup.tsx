1d0084a1fb988cce84956bcc6158b27f0a04e5c5
foreignseal <goodnight.isma@gmail.com>
2026-02-15 18:45:49 +0000

    style: enhance ClosePopup component with improved styling and layout

--- src/components/ClosePopup.tsx
+++ src/components/ClosePopup.tsx
@@ -3,12 +3,12 @@ import { getCurrentWindow } from "@tauri-apps/api/window";
 export default function ClosePopup({ onClose }: { onClose: () => void }) {
 
   return (
-    <div className="pop-up">
-      <div className="content">
-        <h2>Are you sure you want to close the app?</h2>
+    <div className="pop-up" onClick={onClose}>
+      <div className="content" onClick={(e) => e.stopPropagation()}>
+        <h2 className="google-sans-g100">Are you sure you want to close the app?</h2>
         <div className="buttons">
-          <button onClick={() => getCurrentWindow().close()}>Yes</button>
-          <button onClick={onClose}>No</button>
+          <button className="close google-sans-600" onClick={() => getCurrentWindow().close()}>Yes</button>
+          <button className="no-close google-sans-600" onClick={onClose}>No</button>
         </div>
       </div>
     </div>
