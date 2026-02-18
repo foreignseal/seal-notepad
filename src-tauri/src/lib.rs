2cf8b5e5bd4fd94edd6698c2cfcab15cc83aec8f
foreignseal <goodnight.isma@gmail.com>
2026-02-18 18:38:41 +0000

    fix: remove unused tauri_plugin_path from run function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,6 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_path::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
